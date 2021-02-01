e3d45142a474310a6e56da480533670e70a123cd
Si HaMaDa <nrookman_27@yahoo.com>
2021-02-01 21:32:58 +0200

    F Edits with config example

---
+++ config.example.js
@@ -0,0 +1,5 @@
+module.exports = {
+    MONGODB: "mongodb://127.0.0.1:27017/?gssapiServiceName=mongodb",
+    // MONGODB: 'mongodb+srv://username:password@cluster.xxxxx.mongodb.net/test?retryWrites=true&w=majority',
+    SECRET_KEY: "TRY SECRET KEY",
+};
